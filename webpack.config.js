87ff2f2e0509826604e494e46b24911cc4a7d854
Tomas Trajan <tomas.trajan@gmail.com>
2015-08-13 00:23:23 +0200

    refactor(webpack.config): unify distribution of params between default and target specific, use color in build info console output

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
 var _ = require('lodash');
 var minimist = require('minimist');
+var chalk = require('chalk');
+
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var paramsDefault = {
-    debug: true,
-    devtool: 'source-map',
     output: {
         path: './build',
-        filename: '[name].[hash].js'
+        filename: '[name].[hash].js',
+        sourceMapFilename: '[name].[hash].map'
     },
     server: {
         port: 8081
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist', 'build'])
-    ]
+    }
 };
 var paramsPerTarget = {
     DEV: {
-        //devtool: 'eval-source-map',
+        debug: true,
+        devtool: 'inline-source-map',
         output: {
             filename: '[name].js'
-        },
-        plugins: []
+        }
     },
     BUILD: {
-        output: {
-        }
+        debug: true,
+        devtool: 'source-map',
+        plugins: [
+            new CleanWebpackPlugin(['build'])
+        ]
     },
     PROD: {
+        debug: false,
         output: {
             path: './dist'
-        }
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist'])
+        ]
     }
 };
 var TARGET = minimist(process.argv.slice(2)).TARGET || 'BUILD';
@@ -48,7 +53,8 @@ module.exports = {
     },
     output: {
         path: params.output.path,
-        filename: params.output.filename
+        filename: params.output.filename,
+        sourceMapFilename: params.output.sourceMapFilename
     },
     module: {
         loaders: [
@@ -61,7 +67,7 @@ module.exports = {
             template: './src/index.html',
             inject: 'body'
         })
-    ].concat(params.plugins),
+    ].concat(params.plugins || []),
     devServer: {
         port: params.server.port
     },
@@ -72,10 +78,10 @@ module.exports = {
 };
 
 function printBuildInfo() {
-    console.log('\nStarting "' + TARGET + '" build');
+    console.log('\nStarting ' + chalk.bold.green('"' + TARGET + '"') + ' build');
     if (TARGET === 'DEV') {
-        console.log('Dev server: http://localhost:' + params.server.port +
-            '/webpack-dev-server/index.html\n\n');
+        console.log('Dev server: ' +
+            chalk.bold.yellow('http://localhost:' + params.server.port + '/webpack-dev-server/index.html') + '\n\n');
     } else {
         console.log('\n\n');
     }
